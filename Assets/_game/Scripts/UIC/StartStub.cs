bd5ac5c2a4b7f1ac771d05e4bec6d687a5ae72af
UnVoiding <RomenoEx@gmail.com>
2021-07-16 20:08:40 +0300

    fix dialogues, added sprite atlas reducing build size

---
+++ Assets/_game/Scripts/UIC/StartStub.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RomenoCompany
+{
+    public class StartStub : MonoBehaviour
+    {
+        private void Start()
+        {
+            SceneManager.LoadScene("Start", LoadSceneMode.Single);
+        }
+    }
+}
